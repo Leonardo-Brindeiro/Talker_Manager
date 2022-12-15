3089643f9d645acaf547017675a7c54c050f9d21
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-12-15 19:46:48 -0300

    req 01

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const express = require('express');
+const express = require('express', 'readtalker');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +13,11 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (_req, res) => { 
+  const talker = JSON.parse(await fs.readFile(path.resolve('./src/talker.json'), 'utf-8'));
+  return res.status(200).json(talker);
+});
+
 app.listen(PORT, () => {
-  console.log('On');
+  console.log('Online');
 });
