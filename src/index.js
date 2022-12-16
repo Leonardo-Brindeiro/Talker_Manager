5a99f30be1abeca20d5cc29fe7a8d90053590c0e
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-12-16 19:21:28 -0300

    req 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express', 'readtalker');
 const fs = require('fs').promises;
 const path = require('path');
 
-const app = express();
+const app = express(); // essa função cria um servidor web http
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
@@ -18,6 +18,14 @@ app.get('/talker', async (_req, res) => {
   return res.status(200).json(talker);
 });
 
+app.get('/talker/:id', async (req, res) => { // AJUDA DO MEU AMIGO JHONATAS ANICEZIO
+  const { id } = req.params;
+  const talker = JSON.parse(await fs.readFile(path.resolve('./src/talker.json'), 'utf-8'));
+  const talkerid = talker.find((e) => e.id === Number(id));  
+  if (!talkerid) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talkerid);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
-});
+});// 
