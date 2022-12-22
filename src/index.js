3212c87af95afc216c437ab42362bda4848f5dea
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-12-22 17:21:09 -0300

    req3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const express = require('express', 'readtalker');
+const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
+const crypto = require('crypto');
 
 const app = express(); // essa função cria um servidor web http
 app.use(express.json());
@@ -24,6 +25,11 @@ app.get('/talker/:id', async (req, res) => { // AJUDA DO MEU AMIGO JHONATAS ANIC
   const talkerid = talker.find((e) => e.id === Number(id));  
   if (!talkerid) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   return res.status(200).json(talkerid);
+});
+
+app.post('/login', async (_req, res) => { // resolvi usar a biblioteca require e chamar o crypto, ultilizei uma constante de token pra depois ultilizar a minha biblioteca
+    const token = crypto.randomBytes(8).toString('hex');
+  return res.status(200).json({ token }); // {token} serve para eu poder chamar como string
 });
 
 app.listen(PORT, () => {
