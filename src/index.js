e8ce0db59a17fbe140df212571f25b3b1370cf68
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2023-01-03 10:55:33 -0300

    req 7 e 8

--- src/index.js
+++ src/index.js
@@ -23,15 +23,29 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+// ajuda do meu amigo lucas e course ]
+// https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326
+// module/94d0e996-1827-4fbc-bc24-c99fb592925b/section/2ed87e4f-9049-4314-8091-8f71b1925cf6/day/4982a599-9832-419e-a96b-3fe1db634c3e/lesson/dd477b54-5e75-489c-aed9-537623abb326/solution
+app.get('/talker/search', tokenn, async (req, res) => {
+  const { q } = req.query;
+  const newPalestr = JSON.parse(await
+    fs.readFile(path.resolve(newdirectorio), 'utf-8'));
+
+    const indexx = newPalestr.filter((element) => element.name.includes(q));
+  if (!indexx) {
+    return res.status(200).json([]);
+  }
+  return res.status(200).json(indexx);
+});
 
 app.get('/talker', async (_req, res) => {
-  const talker = JSON.parse(await fs.readFile(path.resolve('./src/talker.json'), 'utf-8'));
+  const talker = JSON.parse(await fs.readFile(path.resolve(newdirectorio), 'utf-8'));
   return res.status(200).json(talker);
 });
 
 app.get('/talker/:id', async (req, res) => { // AJUDA DO MEU AMIGO JHONATAS ANICEZIO
   const { id } = req.params;
-  const talker = JSON.parse(await fs.readFile(path.resolve('./src/talker.json'), 'utf-8'));
+  const talker = JSON.parse(await fs.readFile(path.resolve(newdirectorio), 'utf-8'));
   const talkerid = talker.find((e) => e.id === Number(id));
   if (!talkerid) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   return res.status(200).json(talkerid);
@@ -46,7 +60,7 @@ app.post('/talker', tokenn, namevalid, agevalid, talkval, validwatch, ratesVal,
    async (req, res) => {
      const { name, age, talk } = req.body; // vai pegar no meu arquivo json as propriedades 
      const newPalestrante = JSON.parse(await
-       fs.readFile(path.resolve('./src/talker.json'), 'utf-8'));
+       fs.readFile(path.resolve(newdirectorio), 'utf-8'));
    const palestrante = {
      id: newPalestrante[newPalestrante.length - 1].id + 1, // propriedades ajustadas 
      name,
@@ -58,18 +72,30 @@ app.post('/talker', tokenn, namevalid, agevalid, talkval, validwatch, ratesVal,
              return res.status(201).json(palestrante);
   });
 // Os meus midlewares ja estão criados então é só mudar post para push 
+// https:// app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/94d0e996-1827-4fbc-bc24-c99fb592925b/section/2ed87e4f-9049-4314-8091-8f71b1925cf6/day/4982a599-9832-419e-a96b-3fe1db634c3e/lesson/9caf3f05-59f1-4959-8f92-bfe0ff66f49c/solution
   app.put('/talker/:id', tokenn, namevalid, agevalid, talkval, validwatch, ratesVal, 
    async (req, res) => {   
     const { id } = req.params;
     const { name, age, talk } = req.body;
     const newPalest = JSON.parse(await
-      fs.readFile(path.resolve('./src/talker.json'), 'utf-8'));
+      fs.readFile(path.resolve(newdirectorio), 'utf-8'));
       const index = newPalest.findIndex((element) => element.id === Number(id));
       newPalest[index] = { id: Number(id), name, age, talk };
       await fs.writeFile(newdirectorio, JSON.stringify(newPalest)); 
       res.status(200).json(newPalest[index]);
   }); 
- 
+  
+// https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/94d0e996-1827-4fbc-bc24-c99fb592925b/section/2ed87e4f-9049-4314-8091-8f71b1925cf6/day/4982a599-9832-419e-a96b-3fe1db634c3e/lesson/9caf3f05-59f1-4959-8f92-bfe0ff66f49c/solution
+  app.delete('/talker/:id', tokenn,  
+   async (req, res) => {  
+    const { id } = req.params;
+     const newPalestt = JSON.parse(await
+      fs.readFile(path.resolve(newdirectorio), 'utf-8'));
+      const index = newPalestt.filter((idp) => idp.id !== Number(id));
+      await fs.writeFile(newdirectorio, JSON.stringify(index, null, 2)); 
+     return res.status(204).json();
+  }); // ajuda do meu amigo lucas mais course  
+
 app.listen(PORT, () => {
   console.log('Online');
 });//
